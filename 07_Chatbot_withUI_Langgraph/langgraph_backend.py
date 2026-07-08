4b855c88dad7ed2c6bcebe6be02a05a487e3ce81
Abubakar <142691379+Abubakar230@users.noreply.github.com>
2026-07-09 00:36:08 +0500

    streaming

--- 07_Chatbot_withUI_Langgraph/langgraph_backend.py
+++ 07_Chatbot_withUI_Langgraph/langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -31,3 +31,19 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# first streamlit
+# config1 = {"configurable": {"thread_id": "1"}}
+# chatbot.invoke({'messages': [HumanMessage(content='capital of France?')]}, config=config1)
+
+# chatbot.get_state(config1)
+# list(chatbot.get_state_history(config1))
+
+
+# Second streamlit with streaming
+# for message_chunk, metadata in chatbot.stream(
+#     {'messages': [HumanMessage(content='capital of France?')]},
+#     config={'configurable': {'thread_id': 'thread-1'}},
+#     stream_mode='messages',
+# ):
+#     print(message_chunk)
